91661031f411388368efcde93d9f4c9bd8e7ff34
tomech <109527747+tomech4@users.noreply.github.com>
2025-11-25 22:20:14 +0100

    complete function copy_file

--- app/main.py
+++ app/main.py
@@ -1,1 +1,13 @@
-# write your code here
+def copy_file(command: str) -> None:
+    try:
+        arguments = command.split(" ")
+        cmd = arguments[0]
+        old_file = arguments[1]
+        new_file = arguments[2]
+        if cmd != "cp" or old_file == new_file:
+            return None
+        with open(old_file, "r") as file_in, open(new_file, "w") as file_out:
+            old_file_text = file_in.read()
+            file_out.write(old_file_text)
+    except (FileNotFoundError, IndexError):
+        return None
