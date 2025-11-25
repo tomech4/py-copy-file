e8f2fa2d1261224b56e9dee37ce6b70d6b95a8d6
tomech <109527747+tomech4@users.noreply.github.com>
2025-11-25 22:26:22 +0100

    fix command lenght validation

--- app/main.py
+++ app/main.py
@@ -1,13 +1,17 @@
 def copy_file(command: str) -> None:
     try:
         arguments = command.split(" ")
+        if len(arguments) < 3:
+            return None
+
         cmd = arguments[0]
         old_file = arguments[1]
         new_file = arguments[2]
         if cmd != "cp" or old_file == new_file:
             return None
+
         with open(old_file, "r") as file_in, open(new_file, "w") as file_out:
             old_file_text = file_in.read()
             file_out.write(old_file_text)
-    except (FileNotFoundError, IndexError):
+    except FileNotFoundError:
         return None
